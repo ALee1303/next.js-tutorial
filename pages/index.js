4d98bb577a58ea00d6394064f62754538a642f3f
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-05-01 00:59:21 -0500

    create list

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,12 @@ const PostLink = props => {
 export default function Index() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   );
 }
