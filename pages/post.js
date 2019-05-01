63f55c42862764465b544f80ff05f70d3847a626
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-05-01 02:02:28 -0500

    change to display batman show list

--- pages/post.js
+++ pages/post.js
@@ -3,7 +3,7 @@ import Layout from '../components/MyLayout.js';
 
 const Page = withRouter(props => (
   <Layout>
-    <h1>{props.router.query.title}</h1>
+    <h1>{props.router.query.id}</h1>
     <p>This is the blog post content.</p>
   </Layout>
 ));
